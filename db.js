063ab00ec851bc2b7affb9e2c062108141966148
Nico Hairetdin <hairetdin.nico@gmail.com>
2018-07-19 13:40:03 +0300

    sort basedata by ENERC

--- db.js
+++ db.js
@@ -7,7 +7,15 @@ const pool = mysql.createPool({
     password: 'root',
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0//,
+    // typeCast: function (field, next) {
+    //     if (field.type.includes("DECIMAL")) {
+    //     	//console.log(true)
+    //         var value = field.string();
+    //         return (value === null) ? 0 : parseFloat(value);
+    //     }
+    //     return next();
+    // }
 })
 
 module.exports = pool
--- routerBasedata.js
+++ routerBasedata.js
@@ -7,13 +7,14 @@ router.get('/food', async (req, res, next) => {
   const q1 = `SELECT * FROM base;`
   const q2 = 'SELECT foodname FROM `base` WHERE `foodid` = 34611'
   const [rows, fields] = await db.query(q1)
-  //console.log(rows)
+  //console.log(rows.sort((a, b) => parseFloat(b.ENERC) - parseFloat(a.ENERC)))
   res.json(rows)
 })
 
 router.get('/components', async (req, res, next) => {
   const query = `SELECT * FROM ravintotekija_yksikko_luokka;`
   const [rows, fields] = await db.query(query)
+  //console.log(rows)
   res.json(rows)
 })
 
