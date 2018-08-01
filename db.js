20191e8aadde5ede7aedfedc039cf666a2de70f6
Nico Hairetdin <hairetdin.nico@gmail.com>
2018-08-01 15:20:47 +0300

    route for login

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config()
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
+    user: process.env.DB_USER,
     database: 'fineli',
-    password: 'root',
+    password: process.env.DB_PASS,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0//,
--- routerUser.js
+++ routerUser.js
@@ -6,10 +6,13 @@ const validator = require("email-validator")
 const tokenForUser = require('./tokenForUser')
 require('dotenv').config()
 
+// register
 router.post('/', async (req, res) => {
   try {
     const body = req.body
-    if (validateCredentials(body)) {
+    let errorMsg = validateCredentials(body.email) ? '' : 'Virheellinen sähköposti.'
+    errorMsg += body.password.length > 5 ? '' : ' Liian lyhyt salasana.'
+    if (errorMsg.length === 0) {
       const user = {
         email: body.email,
         passhash: await bcrypt.hash(body.password, 10)
@@ -20,7 +23,7 @@ router.post('/', async (req, res) => {
       console.log("user added with id", results.insertId)
       res.json({ token, email: body.email })
     } else {
-      res.status(401).json({ error: 'Virheellinen käyttäjätunnus tai salasana.' })
+      res.status(401).json({ error: errorMsg })
     }
   } catch (e) {
     let msg = "error"
@@ -31,8 +34,34 @@ router.post('/', async (req, res) => {
   }
 })
 
-const validateCredentials = (creds) => {
-  return validator.validate(creds.email) && creds.password.length > 5
+// login
+router.post('/session', async (req, res) => {
+  console.log("login attempt",req.body)
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send()
+  }
+
+  try {
+    const [result, fields] = await db.query('SELECT id, email, passhash FROM user WHERE email = ?', req.body.email)
+
+    if(result.length === 0) {
+      return res.status(401).json({ error: 'Sähköposti ei ole rekisteröity.'})
+    }
+    if(!await bcrypt.compare(req.body.password, result[0].passhash)) {
+      return res.status(401).json({ error: 'Virheellinen salasana.'})
+    }
+
+    const token = tokenForUser.sign({ email: req.body.email, id: result[0].id })
+    res.status(200).json({ token, email: req.body.email })
+  } catch (e) {
+    res.status(503).json({ msg: 'Virhe palvelussa, yritä myöhemmin uudelleen.'})
+  }
+
+  //res.status(200).json({ msg: 'valid' })
+})
+
+const validateCredentials = (email) => {
+  return validator.validate(email)
 }
 
 module.exports = router
